5ba4ac95b17167584d643553ca393864b4b302bf
dalcobe <80398474+dalcobe@users.noreply.github.com>
2021-12-10 20:02:26 +0100

    Add files via upload

---
+++ projecte/connexio.php
@@ -0,0 +1,79 @@
+<?php
+  $connexio = new mysqli ("localhost", "root", "Alastor666Radio", "electroimp");
+?>
+<!DOCTYPE html>
+<html lang="ca" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Filtrar Productes</title>
+    <link rel="stylesheet" href="css/generics.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/taula.css">
+  </head>
+  <body>
+    <header>
+      <h1>Botiga de components</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Pàgina d'Inici</a></li>
+        <li><a href="placa_base/inici.html">Placa Base</a></li>
+        <li><a href="cpu/inici.html">Processadors</a></li>
+        <li><a href="ram/inici.html">Memòria RAM</a></li>
+        <li><a href="disc_dur/inici.html">Disc Durs</a></li>
+        <li><a href="connexio.php">Filtrar Productes</a></li>
+      </ul>
+    </nav>
+    <main>
+      <h2 class="subtitol">Filtrar per categories</h2>
+      <div class="filtres_a">
+        <a class="a_component_index" href="consultes/placabase.php">Plaques Base</a>
+        <a class="a_component_index" href="consultes/processador.php">Processador</a>
+      </div><br>
+      <div class="taula_centrada">
+        <table>
+          <tr>
+            <th>
+              Codi Producte
+            </th>
+            <th>
+              Marca
+            </th>
+            <th>
+              Model
+            </th>
+            <th>
+              Estoc
+            </th>
+            <th>
+              Categoria
+            </th>
+          </tr>
+          <?php
+          $sql="SELECT * from productes,categoria where productes.codi_cat=categoria.codi_cat";
+          $result=mysqli_query($connexio,$sql);
+          while ($mostrar=mysqli_fetch_array($result)) {
+            ?>
+            <tr>
+              <td><?php echo $mostrar['codi_prod']; ?></td>
+              <td><?php echo $mostrar['marca']; ?></td>
+              <td><?php echo $mostrar['model']; ?></td>
+              <td><?php echo $mostrar['estoc']; ?></td>
+              <td><?php echo $mostrar['nom']; ?></td>
+            </tr>
+          <?php
+          }
+          ?>
+
+        </table>
+      </div>
+
+
+      <h2 class="subtitol">Filtrar per tipus de material</h2>
+
+      <h2 class="subtitol">Ordenar productes</h2>
+
+    </main>
+  </body>
+</html>
---
+++ projecte/consultes/placabase.php
@@ -0,0 +1,70 @@
+<?php
+  $connexio = new mysqli ("localhost", "root", "Alastor666Radio", "electroimp");
+?>
+<!DOCTYPE html>
+<html lang="ca" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Filtrar Productes</title>
+    <link rel="stylesheet" href="../css/generics.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/taula.css">
+  </head>
+  <body>
+    <header>
+      <h1>Botiga de components</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="../index.html">Pàgina d'Inici</a></li>
+        <li><a href="../placa_base/inici.html">Placa Base</a></li>
+        <li><a href="../cpu/inici.html">Processadors</a></li>
+        <li><a href="../ram/inici.html">Memòria RAM</a></li>
+        <li><a href="../disc_dur/inici.html">Disc Durs</a></li>
+        <li><a href="../connexio.php">Filtrar Productes</a></li>
+      </ul>
+    </nav>
+    <main>
+      <h2 class="subtitol">Filtrar per Plaques Bases</h2>
+      <div class="taula_centrada">
+        <table>
+          <tr>
+            <th>
+              Codi Producte
+            </th>
+            <th>
+              Marca
+            </th>
+            <th>
+              Model
+            </th>
+            <th>
+              Estoc
+            </th>
+            <th>
+              Categoria
+            </th>
+          </tr>
+          <?php
+          $sql="SELECT * from productes,categoria where productes.codi_cat=categoria.codi_cat and categoria.codi_cat like 1";
+          $result=mysqli_query($connexio,$sql);
+          while ($mostrar=mysqli_fetch_array($result)) {
+            ?>
+            <tr>
+              <td><?php echo $mostrar['codi_prod']; ?></td>
+              <td><?php echo $mostrar['marca']; ?></td>
+              <td><?php echo $mostrar['model']; ?></td>
+              <td><?php echo $mostrar['estoc']; ?></td>
+              <td><?php echo $mostrar['nom']; ?></td>
+            </tr>
+          <?php
+          }
+          ?>
+
+        </table>
+      </div>
+
+    </main>
+  </body>
+</html>
